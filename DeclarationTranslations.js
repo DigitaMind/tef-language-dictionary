e2cd9fcc3d87142172890c7e7d5d7a40f0bc5c92
digitamind <abdullahi.monsur.olalekan@gmail.com>
2020-12-28 19:40:18 +0100

    updated declaration

---
+++ DeclarationTranslations.js
@@ -0,0 +1,148 @@
+export const DeclarationTranslations = {
+  en: {
+    title: "declaration",
+    instruction:
+      "The next set of questions are declaration statements. Please answer with Yes or No",
+    one: "Have you applied for the TEFConnect Programme previously?",
+    oneSub: "If yes, which year(s)?",
+    two:
+      "Have you received seed capital from the TEFConnect Programme in previous cycles?",
+    twoSub: "If yes, which year(s)?",
+    three:
+      "Do you have business partners or collaborators who are Alumnus of the Programme?",
+    four:
+      "Are you a sanctioned person or / a business partner of / or a close relative/ or a friend of a sanctioned person or group? (Sanction Meaning: Punitive or restrictive measures imposed on a country, person or business)",
+    five:
+      "Do you, your business partner or your company have any direct/indirect dealings involving a sanctioned country or a sanctioned person/group?",
+    six:
+      "Are you a politically exposed person or a close relative/friend of a politically exposed person? (Political Exposed Person Meaning: one who has held prominent public/political office or publicly shown interest in holding prominent public/political office)",
+    seven:
+      "Have you or your business partner(s) ever been investigated, arrested or penalised for any crime or terrorist related charges?",
+    eight: "",
+    nine: "",
+    ten: "",
+    eleven:
+      "Are you in business with any TEF staff or staff of any TEF affiliated company (TEF affiliated companies Meaning: includes any company in the Heirs Holdings investment portfolio)?",
+    twelve:
+      "Are you a current or previous staff of any TEF affiliated company?",
+    button: "submit application",
+    preview: "Preview Bio and Experience info",
+    yes: "yes",
+    no: "no",
+    thirteen:
+      "Have you or your business partner(s) ever been investigated, arrested or penalized for any terrorist-related charges",
+    fourteen:
+      "Have you or your business partner(s) ever been investigated, arrested, or penalised for any financial related offence",
+    fifteen:
+      "Have you or your business partner(s) ever been investigated, arrested or convicted of any crime?",
+  },
+
+  fr: {
+    title: "La déclaration au fin des questions d'évaluation.",
+    instruction: "Questions avec does réponses Oui ou Non",
+    one: "Avez-vous postulé pour le programmes auparavant?",
+    oneSub: "Si oui,  quelle année",
+    two:
+      "Avez-vous réçu let capital du Programme de TEF dans les cycles passées?",
+    twoSub: "Si oui, quelle année?",
+    three:
+      "Avez-vous des partennaires ou des collaborateurs qui sont les anciens béneficiares du Programme?",
+    four:
+      "Êtes-vous sanctionné/ un partennaire de/ un proche parent de/ ou un(e) ami(e) de quelqu'un sanctionnée ou groupe? (Sanctionné veut dire: Mesures punitives ou restrictive imposées à un pays, une personne et une affaire)",
+    five:
+      "Est-ce que vous, votre partennaire ou votre société avez des rapports directs ou indirects avec des pays sanctionnés ou une personne you groupe  sanctionné?",
+    six:
+      "Êtes-vous une personne politiquement exposée ou boys êtes ami(e) ou parent(e) d'une personne qui est politiquement exposée? (Les personnel exposes politiquement signifie: Celui qui a tenu un poste politique you qui a publiquement montré un intéret à tenir un poste politique)",
+    seven:
+      "Est-ce que vous ou vos partennaires des affaires ont été un sujet d'investigation, arrêtés ou penalisé un crime ou un activity terroristiques?",
+    eight: "",
+    nine: "",
+    ten: "",
+    eleven:
+      "Est-ce que vous êtes en partenariat avec un fonctionnaire de TEF ou un fonctionnaire d'une société liée a TEF ( Les sociétés liées a TEF signifies: y compris aucune société filiale à Heirs Holdings)?",
+    twelve:
+      "Êtes-vous un fonctionnaire actuel ou ancien d'aucune société liée a Tony Elumelu Foundation?",
+    button: "présenter une demande",
+    preview: "Aperçu de la biographie et de l'expérience",
+    yes: "Oui",
+    no: "Non",
+    thirteen:
+      "Vous ou vos partenaires commerciaux avez-vous déjà fait l'objet d'une enquête, d'une arrestation ou d'une sanction pour des accusations liées au terrorisme?",
+    fourteen:
+      "Vous ou vos partenaires commerciaux avez-vous déjà fait l’objet d’une enquête, d’une arrestation ou d’une sanction pour une infraction financière",
+    fifteen:
+      "Avez-vous déjà fait l'objet d'une enquête, d'une arrestation ou d'une condamnation pour un crime, vous ou vos partenaires commerciaux?",
+  },
+  pt: {
+    title: "Declaração no fim das questões de avaliação",
+    instruction: 'As questões com respostas "Si o Não"',
+    one: "Você têm postulado para o programa de TEF antigamente?",
+    oneSub: "Se a resposta é si em que ano?",
+    two: "Têm recebido o capital do Programa de TEF nos ciclos antigos?",
+    twoSub: "Se a resposta é si em que ano?",
+    three:
+      "Você têm parceiros o colaboradores que são beneficiários antigos do Programa?",
+    four:
+      "Você é uma pessoa sancionada o tinha sancionado o seu parceiro/ um parente próximo/ o têm relações com uma pessoa o um grupo sancionado? (Sanção quer dizer: Medidas punitivas o restritiva imponha a um país, uma pessoa e um negócio)",
+    five:
+      "Você o seu parceiro o a sua empresa têm relações diretas e indiretas com um país sancionado,  uma pessoa sancionada o um grupo sancionado.",
+    six:
+      "Você é uma pessoa exposta o têm um parente o amigo que é politicamente espostas? (Politicamente exposta significa: alguém que têm ocupado um posto público importante o têm mostrado um interesse forte para alcançar a um)",
+    seven:
+      "Você têm parceiros sob a investigação, parceiros presos o condenados por um crime o atividades terroristicas?",
+    eight: "",
+    nine: "",
+    ten: "",
+    eleven:
+      "Você está negociando com algum funcionário de TEF o um funcionário de uma empresa afiliada ao TEF?  (As empresas afilhadas ao TEF significa: compreende qualquer empresa sob o grupo Heirs Holdings)?",
+    twelve:
+      "Você é um funcionário actual o antigo de uma empresa afilliada ao TEF?",
+    button: "enviar inscrição",
+    preview: "Visualizar informações de biografia e experiência",
+    yes: "Si",
+    no: "Não",
+    thirteen:
+      "Você ou seu (s) parceiro (s) de negócios já foi investigado, preso ou penalizado por quaisquer acusações relacionadas ao terrorismo?",
+    fourteen:
+      "Você ou seu (s) parceiro (s) de negócios já foram investigados, presos ou penalizados por qualquer ofensa financeira relacionada?",
+    fifteen:
+      "Você ou seu (s) parceiro (s) de negócios já foram investigados, presos ou condenados por algum crime?",
+  },
+  ar: {
+    title: "declaration",
+    instruction:
+      "The next set of questions are declaration statements. Please answer with Yes or No",
+    one: "Have you applied for the TEFConnect Programme previously?",
+    oneSub: "If yes, which year(s)?",
+    two:
+      "Have you received seed capital from the TEFConnect Programme in previous cycles?",
+    twoSub: "If yes, which year(s)?",
+    three:
+      "Do you have business partners or collaborators who are Alumnus of the Programme?",
+    four:
+      "Are you a sanctioned person or / a business partner of / or a close relative/ or a friend of a sanctioned person or group? (Sanction Meaning: Punitive or restrictive measures imposed on a country, person or business)",
+    five:
+      "Do you, your business partner or your company have any direct/indirect dealings involving a sanctioned country or a sanctioned person/group?",
+    six:
+      "Are you a politically exposed person or a close relative/friend of a politically exposed person? (Political Exposed Person Meaning: one who has held prominent public/political office or publicly shown interest in holding prominent public/political office)",
+    seven:
+      "Have you or your business partner(s) ever been investigated, arrested or penalised for any crime or terrorist related charges?",
+    eight: "",
+    nine: "",
+    ten: "",
+    eleven:
+      "Are you in business with any TEF staff or staff of any TEF affiliated company (TEF affiliated companies Meaning: includes any company in the Heirs Holdings investment portfolio)?",
+    twelve:
+      "Are you a current or previous staff of any TEF affiliated company?",
+    button: "submit application",
+    preview: "Preview Bio and Experience info",
+    yes: "yes",
+    no: "no",
+    thirteen:
+      "Have you or your business partner(s) ever been investigated, arrested or penalized for any terrorist-related charges",
+    fourteen:
+      "Have you or your business partner(s) ever been investigated, arrested, or penalised for any financial related offence",
+    fifteen:
+      "Have you or your business partner(s) ever been investigated, arrested or convicted of any crime?",
+  }
+};
