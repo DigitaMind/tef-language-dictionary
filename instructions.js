7d16ea7556c93ea7df07677e7da6d446d38c4ead
digitamind <abdullahi.monsur.olalekan@gmail.com>
2020-12-28 20:07:46 +0100

    added application instruction

---
+++ instructions.js
@@ -0,0 +1,98 @@
+export const instructions = {
+  en: {
+    title: "Guidelines for the Assessment.",
+    subHeading: "Read the following carefully",
+    instruction1:
+      "This assessment is divided into four (4) sections and would require approximately 45mins to complete. It is recommended that you take the assessment in an environment free of distractions and with optimal concentration.",
+    instruction2:
+      "Please note that you are to answer all questions and you can only take this assessment once.",
+    instruction3:
+      "You will also be required to upload a form of ID (international passport, driver’s license, Residency or Citizen’s Card, Voters Card, Tax ID). Please have your chosen form of ID scanned and ready for upload in .pdf, .jpeg, .jpg, .png, .gif or .doc format before you begin.",
+    list: "Automatic Disqualification criteria:",
+    list1: "If you are under 18years of age",
+    list2:
+      "If you have received a grant or seed capital from TEF in previous years",
+    next: 'Click the "Next" button to begin.',
+  },
+  fr: {
+    title: "Informations sur le processus de candidature",
+    subHeading: "Veuillez lire attentivement les informations suivantes:",
+    instruction1:
+      "Cette évaluation est divisée en 4 sections et nécessitera approximativement 45 minutes pour être complétée. Nous vous recommandons de compléter cette évaluation dans un endroit sans distraction et où vous pourrez vous concentrer.",
+    instruction2:
+      "Merci de noter que vous devez répondre à toutes les questions et vous ne pouvez compléter l’évaluation qu’une seule fois.",
+    instruction3:
+      "Vous devrez télécharger une pièce d'identité (passeport, permis de conduire, Carte Nationale d’Identité, etc.). Nous vous recommandons de scanner votre pièce d’identité et de la télécharger au format .pdf, .jpeg, .jpg, .png, .gif ou .doc avant de commencer.",
+    list:
+      "Dans les deux cas suivants, votre candidature ne sera pas prise en compte:",
+    list1: "Candidat.e.s en dessous de l'âge minimum de 18 ans",
+    list2:
+      "Candidat.e.s ayant reçu une subvention TEF dans un cycle précédent.",
+    next: "Cliquez sur le bouton “Suivant” pour commencer.",
+  },
+  pt: {
+    title: "Guia pela avaliação.",
+    subHeading: "Léia cuidadosamente a seguinte",
+    instruction1:
+      "A avaliação é dividida nas 4 seções e precisará aproximadamente 45 minutos para completar. E recomendado tomar a avaliação num ambiente livre das distrações e com uma concentração ótima.",
+    instruction2:
+      "Por favor, note bem que você têm que responder a todas as questões e pode somente tomar a avaliação uma vez.",
+    instruction3:
+      "É requerido abaixar uma forma de ID ( passaporte internacional, permiso de motorista, Carta de Cidadania/Residência, Carta de Votador, ID de Impostos) Por favor, a sua ID escolhida deve ser escaneada e abaixada no formato .pdf, .jpeg, .jpg, .png, .gif or .doc antes de começar.",
+    list: "O critério de desqualificação automática:",
+    list1: "Candidatos sob 18 de anos.",
+    list2: "Candidatos que têm recebido uma subvenção de TEF nos anos antigos.",
+    next: 'Clique a tecla "Próximo” para começar',
+  },
+  ar: {
+    title: "",
+    subHeading: "",
+    instruction1: "",
+    instruction2: "",
+    instruction3: "",
+    list: "",
+    list1: "",
+    list2: "",
+    next: "",
+  },
+};
+
+export const sectionOneTranslations = {
+  en:
+    "This set of questions are about you. Please ensure that the personal details you provide match the information on your proof of ID.",
+  fr:
+    "Cette série de questions est à propos de vous. Veuillez-vous assurer que les informations personnelles que vous fournissez correspondent aux informations figurant sur votre pièce d'identité.",
+  pt:
+    "Por favor, assegure que os detalhes pessoais que someteu contém as mesmas informações na sua ID.",
+  ar: "",
+};
+
+export const sectionTwoTranslations = {
+  en:
+    "The next set of questions are about your business idea and experience. Please provide complete and accurate responses.",
+  fr:
+    "La prochaine série de questions porte sur votre idée d'entreprise et votre expérience. Veuillez fournir des réponses complètes et précises.",
+  pt:
+    "As próximas questões se tratam da sua idéia de negócio e a sua experiência. Você tem que escolher a resposta mais correta.",
+  ar: "",
+};
+
+export const sectionThreeTranslations = {
+  en:
+    "In each of the following questions is a statement. You are to select the best option that describes your opinion of the statement. Please answer all questions honestly.",
+  fr:
+    "Dans chacune des questions suivantes se trouve une déclaration. Vous devez sélectionner la meilleure option qui décrit votre opinion sur la déclaration. Veuillez répondre honnêtement à toutes les questions.",
+  pt:
+    "Em cada uma das perguntas a seguir há uma declaração. Você deve selecionar a melhor opção que descreve sua opinião sobre a declaração. Por favor, responda todas as perguntas honestamente.",
+  ar: "",
+};
+
+export const sectionFourTranslations = {
+  en:
+    "This section contains some business and financial questions. You are to choose the most correct answer.",
+  fr:
+    "Cette section contient quelque questions d’affaire de base. Vous choisirez la meilleure réponse de toutes les options chaque fois.",
+  pt:
+    "Está seção contém qualquer questões de negócios básicos e têm que escolher a melhor resposta de todas as opções cada vez.",
+  ar: "",
+};
